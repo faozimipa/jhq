04d87e1dd8fd6b56089da2b04b810420f1874562
Faozi <faozimipa@gmail.com>
2021-06-10 19:16:41 +0700

    fixing categoryName

--- src/main/java/id/zitech/base/service/mapper/SubMapper.java
+++ src/main/java/id/zitech/base/service/mapper/SubMapper.java
@@ -1,6 +1,5 @@
 package id.zitech.base.service.mapper;
 
-
 import id.zitech.base.domain.*;
 import id.zitech.base.service.dto.SubDTO;
 
@@ -9,10 +8,11 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity {@link Sub} and its DTO {@link SubDTO}.
  */
-@Mapper(componentModel = "cdi", uses = {CategoryMapper.class})
+@Mapper(componentModel = "cdi", uses = { CategoryMapper.class })
 public interface SubMapper extends EntityMapper<SubDTO, Sub> {
 
     @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "category.categoryName", target = "categoryName")
     SubDTO toDto(Sub sub);
 
     @Mapping(source = "categoryId", target = "category")
